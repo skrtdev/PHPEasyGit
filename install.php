94156eafd0518edc2cdadaed4dbd44a08348a41c
Gaetano <64538010+skrtdev@users.noreply.github.com>
2020-06-26 17:04:15 +0200

    Update install.php

--- install.php
+++ install.php
@@ -1,16 +1,4 @@
 <?php
-
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary");
-header("Content-disposition: attachment; filename=\"PHPEasyGit_Fast-Installer.php\"");
-
-
-echo '
-<?php
-copy("'.'https://' . $_SERVER['HTTP_HOST'] . "/PHPEasyGitInstaller/installer.php".'", __FILE__);
+copy("https://gdev.ga/PHPEasyGitInstaller/installer.php", __FILE__);
 header("Refresh:0");
-?>
-';
-
 ?>
